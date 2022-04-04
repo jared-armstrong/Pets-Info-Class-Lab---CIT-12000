639280dfa13a6838ad3b8e72e597b2b858cde6d1
jared-armstrong <97423883+jared-armstrong@users.noreply.github.com>
2022-04-04 17:11:15 -0400

    first commit

---
+++ animals.js
@@ -0,0 +1,51 @@
+const petsData = [
+    {
+        petName: "Stella",
+        age: 7,
+        weightInKilos: 24,
+        breed: "Dalmation"
+    },
+    {
+        petName: "Cody",
+        age: 8,
+        weightInKilos: 22,
+        breed: "Corgi"
+    },
+    {
+        petName: "Mango",
+        age: 2,
+        weightInKilos: 11,
+        breed: "Persian"
+    },
+    {
+        petName: "Lucy",
+        age: 4,
+        weightInKilos: 35,
+        breed: "Ball Python"
+    },
+    {
+        petName: "Buhmie",
+        age: 1,
+        weightInKilos: 28,
+        breed: "Bull-dog"
+    }
+];
+
+
+
+const showInfo = () => {
+    console.log("showinfo");
+
+    let petNumber = document.querySelector("#petNum").value;
+    console.log(petNumber);
+
+    petNumber = parseInt(petNumber) - 1;
+
+    let selectedPetInfo = document.querySelector(".selectedPetInfo");
+
+    let pet = petsData[petNumber];
+
+    selectedPetInfo.textContent = `${pet.petName} is a ${pet.breed} and is ${pet.age} years old.`;
+
+    // let petPic = document.querySelector(".selectedPetPic");
+}
